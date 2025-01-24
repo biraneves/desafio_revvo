cd210145de4db8a2b992b27042a8e24e75f21e54
Bira Neves <biraneves@outlook.com>
2025-01-24 17:37:21 -0300

    Add php code to course-list

--- src/views/course-list.php
+++ src/views/course-list.php
@@ -4,6 +4,7 @@
             <div class="container">
                 <h2 class="courses-title">Meus cursos</h2>
                 <div class="courses__grid">
+                    <?php foreach($courseList as $course): ?>
                     <article class="courses__grid__course">
                         <div class="courses__grid__course__img-container">
                             <img src="img/notebook-studying_300.jpg" alt="Course image" class="courses__grid__course__img-container__image">
@@ -17,84 +18,7 @@
                             <button class="courses__grid__course__content-container__button">Ver curso</button>
                         </div>
                     </article>
-                    <article class="courses__grid__course">
-                        <div class="courses__grid__course__img-container">
-                            <img src="img/notebook-studying_300.jpg" alt="Course image" class="courses__grid__course__img-container__image">
-                        </div>
-                        <div class="courses__grid__course__content-container">
-                            <h3 class="courses__grid__course__content-container__title">Pallentesque Malesuada</h3>
-                            <p class="courses__grid__course__content-container__description">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi sequi, vel dignissimos, 
-                                enim dolorem placeat sit molestiae.
-                            </p>
-                            <button class="courses__grid__course__content-container__button">Ver curso</button>
-                        </div>
-                    </article>
-                    <article class="courses__grid__course">
-                        <div class="courses__grid__course__img-container">
-                            <img src="img/notebook-studying_300.jpg" alt="Course image" class="courses__grid__course__img-container__image">
-                        </div>
-                        <div class="courses__grid__course__content-container">
-                            <h3 class="courses__grid__course__content-container__title">Pallentesque Malesuada</h3>
-                            <p class="courses__grid__course__content-container__description">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi sequi, vel dignissimos, 
-                                enim dolorem placeat sit molestiae.
-                            </p>
-                            <button class="courses__grid__course__content-container__button">Ver curso</button>
-                        </div>
-                    </article>
-                    <article class="courses__grid__course">
-                        <div class="courses__grid__course__img-container">
-                            <img src="img/notebook-studying_300.jpg" alt="Course image" class="courses__grid__course__img-container__image">
-                        </div>
-                        <div class="courses__grid__course__content-container">
-                            <h3 class="courses__grid__course__content-container__title">Pallentesque Malesuada</h3>
-                            <p class="courses__grid__course__content-container__description">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi sequi, vel dignissimos, 
-                                enim dolorem placeat sit molestiae.
-                            </p>
-                            <button class="courses__grid__course__content-container__button">Ver curso</button>
-                        </div>
-                    </article>
-                    <article class="courses__grid__course">
-                        <div class="courses__grid__course__img-container">
-                            <img src="img/notebook-studying_300.jpg" alt="Course image" class="courses__grid__course__img-container__image">
-                        </div>
-                        <div class="courses__grid__course__content-container">
-                            <h3 class="courses__grid__course__content-container__title">Pallentesque Malesuada</h3>
-                            <p class="courses__grid__course__content-container__description">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi sequi, vel dignissimos, 
-                                enim dolorem placeat sit molestiae.
-                            </p>
-                            <button class="courses__grid__course__content-container__button">Ver curso</button>
-                        </div>
-                    </article>
-                    <article class="courses__grid__course">
-                        <div class="courses__grid__course__img-container">
-                            <img src="img/notebook-studying_300.jpg" alt="Course image" class="courses__grid__course__img-container__image">
-                        </div>
-                        <div class="courses__grid__course__content-container">
-                            <h3 class="courses__grid__course__content-container__title">Pallentesque Malesuada</h3>
-                            <p class="courses__grid__course__content-container__description">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi sequi, vel dignissimos, 
-                                enim dolorem placeat sit molestiae.
-                            </p>
-                            <button class="courses__grid__course__content-container__button">Ver curso</button>
-                        </div>
-                    </article>
-                    <article class="courses__grid__course">
-                        <div class="courses__grid__course__img-container">
-                            <img src="img/notebook-studying_300.jpg" alt="Course image" class="courses__grid__course__img-container__image">
-                        </div>
-                        <div class="courses__grid__course__content-container">
-                            <h3 class="courses__grid__course__content-container__title">Pallentesque Malesuada</h3>
-                            <p class="courses__grid__course__content-container__description">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi sequi, vel dignissimos, 
-                                enim dolorem placeat sit molestiae.
-                            </p>
-                            <button class="courses__grid__course__content-container__button">Ver curso</button>
-                        </div>
-                    </article>
+                    <?php endforeach; ?>
                     <a href="#" class="card_link">
                         <div class="courses__grid__course-add">
                             <img src="img/add-course.svg" alt="Add course" class="courses__grid__course-add__icon">
