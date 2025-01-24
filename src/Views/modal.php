6e26e1cad40f0dac237d3b25f1ef4a9e55cbee44
Bira Neves <biraneves@outlook.com>
2025-01-24 20:54:37 -0300

    Add Modal HTML code

---
+++ src/Views/modal.php
@@ -0,0 +1,15 @@
+<div class="modal">
+    <div class="modal__content">
+        <button class="modal__content__close-button">&times;</button>
+        <div class="modal__content__header">
+            <img src="img/workspace.jpg" alt="Flat-lay illustration of a workspace">
+        </div>
+        <div class="modal__content__body">
+            <h2 class="modal__content__body__title">Egestas Tortor Vulputate</h2>
+            <p class="modal__content__body__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cum necessitatibus harum accusantium sed repellendus, aperiam assumenda dignissimos!
+            </p>
+            <a href="#" class="modal__content__body__button">Inscreva-se</a>
+        </div>
+    </div>
+</div>
