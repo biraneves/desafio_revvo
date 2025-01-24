b8b36affbec71d57c1ed89f0bc689c66a50f1f41
Bira Neves <biraneves@outlook.com>
2025-01-24 17:48:49 -0300

    Fix typo in the router handler

--- src/Config/routes.php
+++ src/Config/routes.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 use Biraneves\Revvo\Controllers\CourseListController;
 
 return [
-    'GET/|' => CourseListController::class,
+    'GET|/' => CourseListController::class,
 ];
