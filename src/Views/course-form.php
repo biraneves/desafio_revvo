5d211ba3d5ae891b777fe061e815e71a35f485c4
Bira Neves <biraneves@outlook.com>
2025-01-24 18:02:17 -0300

    Add view course-form and class CourseFormController

---
+++ src/Views/course-form.php
@@ -0,0 +1,8 @@
+<?php require_once __DIR__ . '/html-start.php'; ?>
+    <section class="courses">
+        <div class="container">
+            <h2 class="courses-title">Meus cursos</h2>
+            
+        </div>
+    </section>
+<?php require_once __DIR__ . '/html-end.php'; ?>
