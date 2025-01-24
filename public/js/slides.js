fdb2dd354badfe008ccfa3af8b313d1c6d38c468
Bira Neves <biraneves@outlook.com>
2025-01-24 16:05:14 -0300

    Add script to get carousel's elements

---
+++ public/js/slides.js
@@ -0,0 +1,6 @@
+// Elements selection
+const slidesTrack = document.querySelector('.banner__slides');
+const slides = Array.from(slidesTrack.children);
+const previousButton = document.querySelector('.banner__buttons--prev');
+const nextButton = document.querySelector('.banner__buttons--next');
+
