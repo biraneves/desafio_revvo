4d0f2fba09631f81b4673f87baee51f987ecf37c
Bira Neves <biraneves@outlook.com>
2025-01-24 21:38:50 -0300

    Fix carousel misbehavior and add autoplay

--- public/js/slides.js
+++ public/js/slides.js
@@ -1,29 +1,23 @@
-// Elements selection
 const slidesTrack = document.querySelector('.banner__slides');
 const slides = Array.from(slidesTrack.children);
 const previousButton = document.querySelector('.banner__buttons--prev');
 const nextButton = document.querySelector('.banner__buttons--next');
 
-// Get the width of the slides
 const slideWidth = slides[0].getBoundingClientRect().width;
 
-// Arrange the slides next to each other
 slides.forEach((slide, index) => slide.style.left = `${index * slideWidth}px`);
 
-// Update buttons
 const updateButtons = (slides, previousButton, nextButton, targetIndex) => {
     previousButton.disabled = targetIndex === 0;
     nextButton.disabled = targetIndex === slides.length - 1;
 };
 
-// Moving the slide
 const moveToSlide = (slidesTrack, currentSlide, targetSlide) => {
     slidesTrack.style.transform = `translateX(-${targetSlide.style.left})`;
     currentSlide.classList.remove('current-slide');
     targetSlide.classList.add('current-slide');
 };
 
-// Events listeners
 nextButton.addEventListener('click', () => {
     const currentSlide = slidesTrack.querySelector('.current-slide');
     const currentIndex = slides.indexOf(currentSlide);
@@ -40,6 +34,31 @@ previousButton.addEventListener('click', () => {
 
     moveToSlide(slidesTrack, currentSlide, previousSlide);
     updateButtons(slides, previousButton, nextButton, currentIndex - 1);
+});
+
+let autoSlideInterval = setInterval(() => {
+    const currentSlide = slidesTrack.querySelector('.current-slide');
+    const currentIndex = slides.indexOf(currentSlide);
+    const nextIndex = (currentIndex + 1) % slides.length;
+    const nextSlide = slides[nextIndex];
+
+    moveToSlide(slidesTrack, currentSlide, nextSlide);
+    updateButtons(nextIndex);
+}, 3000);
+
+[previousButton, nextButton].forEach((button) => {
+    button.addEventListener('click', () => {
+        clearInterval(autoSlideInterval);
+        autoSlideInterval = setInterval(() => {
+            const currentSlide = slidesTrack.querySelector('.current-slide');
+            const currentIndex = slides.indexOf(currentSlide);
+            const nextIndex = (currentIndex + 1) % slides.length;
+            const nextSlide = slides[nextIndex];
+
+            moveToSlide(slidesTrack, currentSlide, nextSlide);
+            updateButtons(nextIndex);
+        }, 3000);
+    });
 });
 
 // Initial state
